75d7fef533ec88fbfea94ed676006dadd6c530c9
Juan Pablo Garza <jpgarza@outlook.com>
2019-08-08 20:20:31 -0300

    prueba conf

--- conf.py
+++ conf.py
@@ -17,12 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'prueba'
-copyright = '2019, juanpgarza'
-author = 'juanpgarza'
-
-# The full version, including alpha/beta/rc tags
-release = '1'
+project = 'Doc. Usuario Odoo'
+copyright = '2019, juanpgarza.com'
+author = 'Juan Pablo Garza'
 
 
 # -- General configuration ---------------------------------------------------
@@ -54,11 +51,25 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
+
+
+#---sphinx-themes-----
+#import guzzle_sphinx_theme
+#
+#html_theme_path = guzzle_sphinx_theme.html_theme_path()
+#html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+#extensions.append("guzzle_sphinx_theme")
+
+#https://pypi.org/project/sphinx-rtd-theme/#description
+# theme de readTheDocs
+html_theme = "sphinx_rtd_theme"
 
 master_doc = 'index'
